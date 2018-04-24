a70baa98787688b91d4818bee6ad5c5789b217a5
Oleg Rosinskiy <Wiztoonz4@gmail.com>
2018-04-24 18:09:43 +0300

    cos_v0.1

--- src/com/lab/javafx/calculator/cos/CosController.java
+++ src/com/lab/javafx/calculator/cos/CosController.java
@@ -1,4 +1,16 @@
 package com.lab.javafx.calculator.cos;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class CosController {
+    @FXML
+    private TextField firstNumber;
+    @FXML
+    private TextField secondNumber;
+
+    @FXML
+    private void cos_click(ActionEvent event) {
+    }
 }
