706674c4fbe54c27c02ba443ffb8c0cd3224e4c9
Oleg Rosinskiy <Wiztoonz4@gmail.com>
2018-04-24 19:47:31 +0300

    delet me plz

--- src/com/lab/javafx/calculator/cos/CosController.java
+++ src/com/lab/javafx/calculator/cos/CosController.java
@@ -1,16 +1,39 @@
 package com.lab.javafx.calculator.cos;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 
+import java.util.ArrayList;
+
 public class CosController {
+
+    @FXML
+    private TableView<CosController> mainTable;
+    @FXML
+    private TableColumn tableNumber;
+    @FXML
+    private TableColumn tableCosNumber;
+
     @FXML
     private TextField firstNumber;
     @FXML
     private TextField secondNumber;
 
+
     @FXML
-    private void cos_click(ActionEvent event) {
+    protected void cos_click() {
+        ObservableList<CosController> nums = mainTable.getItems();
+        for(int i = 0; i < Integer.parseInt(firstNumber.getText()); i++) {
+            nums.add(new CosController()); // добавляем строку
+        }
     }
+
+    }
+
 }
